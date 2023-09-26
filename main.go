9e5a254b9db27ec7eddb33a22d2dcfb678aac454
catJam <loginerrore04@gmail.com>
2023-09-26 16:59:26 +0800

    [backend] basic routing

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/jykuo-love-shiritori/twp/pkg"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-    fmt.Println("Hello")
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Group("/", middleware.Static("frontend/dist"))
+	pkg.RouterHandler(e.Group("/api"))
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
