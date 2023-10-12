2c043214983e6c4ede6751c27c924eb204b2b0a7
tyk <110590015@ntut.org.tw>
2023-10-12 17:34:49 +0800

    [frontend] set formatter(eslint, prettierrc)

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,11 +4,13 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': 'error',
   },
-}
+};
