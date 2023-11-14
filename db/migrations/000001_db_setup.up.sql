7f3c556bc8880315796fc996e018348628cb0604
catJam <loginerrore04@gmail.com>
2023-11-14 17:44:58 +0800

    [backend] unique name and email

--- db/migrations/000001_db_setup.up.sql
+++ db/migrations/000001_db_setup.up.sql
@@ -93,13 +93,12 @@ CREATE TABLE
         "username" VARCHAR(255) NOT NULL UNIQUE,
         "password" VARCHAR(255) NOT NULL,
         "name" VARCHAR(255) NOT NULL,
-        "email" VARCHAR(255) NOT NULL,
+        "email" VARCHAR(255) NOT NULL UNIQUE,
         "address" VARCHAR(255) NOT NULL,
         "image_id" UUID NOT NULL,
         "role" role_type NOT NULL,
         "session_token" VARCHAR(255) NOT NULL,
         "credit_card" JSONB NOT NULL,
-        UNIQUE ("username", "email")
     );
 
 CREATE TABLE
