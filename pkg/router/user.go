307c782d25dc8c69dc5594a12401eedb4678bf55
catJam <loginerrore04@gmail.com>
2023-11-29 21:36:36 +0800

    [backend] Mediation between go/swag fmt

--- pkg/router/user.go
+++ pkg/router/user.go
@@ -8,14 +8,14 @@ import (
 	"go.uber.org/zap"
 )
 
-//	@Summary		User Get Info
-//	@Description	Get user information
-//	@Tags			User
-//	@Accept			json
-//	@Produce		json
-//	@Success		200
-//	@Failure		401
-//	@Router			/user [get]
+// @Summary		User Get Info
+// @Description	Get user information
+// @Tags			User
+// @Accept			json
+// @Produce		json
+// @Success		200
+// @Failure		401
+// @Router			/user [get]
 func userGetInfo(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
@@ -23,14 +23,14 @@ func userGetInfo(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	}
 }
 
-//	@Summary		User Edit Info
-//	@Description	Edit user information
-//	@Tags			User
-//	@Accept			json
-//	@Produce		json
-//	@Success		200
-//	@Failure		401
-//	@Router			/user/edit [patch]
+// @Summary		User Edit Info
+// @Description	Edit user information
+// @Tags			User
+// @Accept			json
+// @Produce		json
+// @Success		200
+// @Failure		401
+// @Router			/user/edit [patch]
 func userEditInfo(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
@@ -38,15 +38,15 @@ func userEditInfo(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	}
 }
 
-//	@Summary		User Upload Avatar
-//	@Description	Upload user avatar
-//	@Tags			User
-//	@Accept			png,jpeg,gif
-//	@Produce		json
-//	@Param			img	formData	file	true	"Image to upload"
-//	@Success		200
-//	@Failure		401
-//	@Router			/user/avatar [post]
+// @Summary		User Upload Avatar
+// @Description	Upload user avatar
+// @Tags			User
+// @Accept			png,jpeg,gif
+// @Produce		json
+// @Param			img	formData	file	true	"Image to upload"
+// @Success		200
+// @Failure		401
+// @Router			/user/avatar [post]
 func userUploadAvatar(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
@@ -54,14 +54,14 @@ func userUploadAvatar(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	}
 }
 
-//	@Summary		User Edit Password
-//	@Description	Change user password
-//	@Tags			User
-//	@Accept			json
-//	@Produce		json
-//	@Success		200
-//	@Failure		401
-//	@Router			/user/password [post]
+// @Summary		User Edit Password
+// @Description	Change user password
+// @Tags			User
+// @Accept			json
+// @Produce		json
+// @Success		200
+// @Failure		401
+// @Router			/user/password [post]
 func userEditPassword(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
@@ -69,14 +69,14 @@ func userEditPassword(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	}
 }
 
-//	@Summary		User Get Credit Card
-//	@Description	Get all credit cards of the user
-//	@Tags			CreditCard
-//	@Accept			json
-//	@Produce		json
-//	@Success		200
-//	@Failure		401
-//	@Router			/user/security/credit_card [get]
+// @Summary		User Get Credit Card
+// @Description	Get all credit cards of the user
+// @Tags			CreditCard
+// @Accept			json
+// @Produce		json
+// @Success		200
+// @Failure		401
+// @Router			/user/security/credit_card [get]
 func userGetCreditCard(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
@@ -84,15 +84,15 @@ func userGetCreditCard(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	}
 }
 
-//	@Summary		User Delete Credit Card
-//	@Description	Delete a credit card by its ID
-//	@Tags			CreditCard
-//	@Accept			json
-//	@Produce		json
-//	@Param			id	query	int	true	"Credit Card ID"
-//	@Success		200
-//	@Failure		401
-//	@Router			/user/security/credit_card/delete [delete]
+// @Summary		User Delete Credit Card
+// @Description	Delete a credit card by its ID
+// @Tags			CreditCard
+// @Accept			json
+// @Produce		json
+// @Param			id	query	int	true	"Credit Card ID"
+// @Success		200
+// @Failure		401
+// @Router			/user/security/credit_card/delete [delete]
 func userDeleteCreditCard(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
@@ -100,14 +100,14 @@ func userDeleteCreditCard(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc
 	}
 }
 
-//	@Summary		User Add Credit Card
-//	@Description	Add a new credit card for the user
-//	@Tags			CreditCard
-//	@Accept			json
-//	@Produce		json
-//	@Success		200
-//	@Failure		401
-//	@Router			/user/security/credit_card/add [post]
+// @Summary		User Add Credit Card
+// @Description	Add a new credit card for the user
+// @Tags			CreditCard
+// @Accept			json
+// @Produce		json
+// @Success		200
+// @Failure		401
+// @Router			/user/security/credit_card/add [post]
 func userAddCreditCard(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 	return func(c echo.Context) error {
 
