8a5c85d756c31c11ea910c2ecdc301388326cd06
UG <ug00257793@gmail.com>
2023-11-29 15:49:15 +0800

    [frontend] del admin button

--- frontend/src/components/UserTableRow.tsx
+++ frontend/src/components/UserTableRow.tsx
@@ -1,6 +1,6 @@
 import { Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 type UserTableRowProps = {
@@ -15,21 +15,15 @@ type UserTableRowProps = {
 };
 
 const UserTableRow = ({ data, isBigScreen = true }: UserTableRowProps) => {
-  //admin checkbox
-  const [isCheckedAdmin, setIsCheckedAdmin] = useState(data.isAdmin);
-  const toggleIsAdmin = () => {
-    setIsCheckedAdmin(!isCheckedAdmin);
-  };
-
   //delete checkbox
   const ColStyleOn = {
     fontSize: '16px',
-    padding: '1% 1% 0% 1%',
+    padding: '1% 1% 1% 1%',
   };
   const ColStyleOff = {
     fontSize: '16px',
-    padding: '1% 1% 0% 1%',
-    color: 'var(--button_dark)',
+    padding: '1% 1% 1% 1%',
+    color: 'var(--layout)',
     textDecoration: 'line-through',
   };
   const [currentStyle, setCurrentStyle] = useState({ state: true, style: ColStyleOn });
@@ -48,23 +42,14 @@ const UserTableRow = ({ data, isBigScreen = true }: UserTableRowProps) => {
         <Col md={1} className={'center'} style={currentStyle.style}>
           <img src={data.iconUrl} className='user_img' />
         </Col>
-        <Col md={2} className={'left center_vertical'} style={currentStyle.style}>
+        <Col md={3} className={'left center_vertical'} style={currentStyle.style}>
           <h4>{data.name}</h4>
         </Col>
-        <Col md={4} className={'left center_vertical'} style={currentStyle.style}>
+        <Col md={5} className={'left center_vertical'} style={currentStyle.style}>
           <h4>{data.email}</h4>
         </Col>
-        <Col md={3} className={'left center_vertical'} style={currentStyle.style}>
+        <Col md={2} className={'left center_vertical'} style={currentStyle.style}>
           <h4>{data.createDate}</h4>
-        </Col>
-        <Col md={1} className={'center center_vertical'} style={currentStyle.style}>
-          <FontAwesomeIcon
-            icon={faGear}
-            size='2x'
-            color={isCheckedAdmin ? 'auto' : 'black'}
-            onClick={toggleIsAdmin}
-            style={{ cursor: 'pointer' }}
-          />
         </Col>
         <Col md={1} className={'center center_vertical'} style={currentStyle.style}>
           <FontAwesomeIcon
@@ -85,35 +70,17 @@ const UserTableRow = ({ data, isBigScreen = true }: UserTableRowProps) => {
           <Col xs={4} md={4} className={'center'} style={currentStyle.style}>
             <img src={data.iconUrl} className='user_img' />
           </Col>
-          <Col xs={6} md={6} className={'left center_vertical'} style={currentStyle.style}>
+          <Col xs={7} md={7} className={'left center_vertical'} style={currentStyle.style}>
             <Row>
-              <p>
+              <p style={currentStyle.style}>
                 {'name: ' + data.name} <br />
                 {'email: ' + data.email} <br />
                 {'created: ' + data.createDate}
               </p>
             </Row>
           </Col>
-          <Col xs={2} md={2}>
+          <Col xs={1} md={1}>
             <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-              {/* gear (admin) button */}
-              <div
-                className='center center_vertical'
-                style={{
-                  display: 'flex',
-                  flexGrow: '1',
-                  justifyContent: 'center',
-                  ...currentStyle.style,
-                }}
-              >
-                <FontAwesomeIcon
-                  icon={faGear}
-                  size='2x'
-                  color={isCheckedAdmin ? 'auto' : 'black'}
-                  onClick={toggleIsAdmin}
-                  style={{ cursor: 'pointer' }}
-                />
-              </div>
               {/* delete button */}
               <div
                 className='center center_vertical'
