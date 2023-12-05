333d9d9d3c04012b05bd97316c4259f8e5f03190
x06lan <x065p4mp6@gmail.com>
2023-12-06 01:58:55 +0800

    [backend] minio upload file and get url

--- go.mod
+++ go.mod
@@ -14,6 +14,21 @@ require (
 )
 
 require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/sha256-simd v1.0.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/rs/xid v1.5.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
+
+require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-openapi/jsonpointer v0.20.0 // indirect
@@ -30,6 +45,7 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/minio/minio-go/v7 v7.0.65
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/swaggo/files/v2 v2.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
---
+++ pkg/minio/minio_operation.go
@@ -0,0 +1,94 @@
+package minio
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"mime/multipart"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/labstack/echo/v4"
+	"github.com/minio/minio-go/v7"
+	"go.uber.org/zap"
+
+	"github.com/jykuo-love-shiritori/twp/pkg/constants"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+var MC *minio.Client
+
+func init() {
+
+	endpoint := "localhost:" + os.Getenv("MINIO_API_PORT")
+	accessKey := os.Getenv("MINIO_ACCESS_KEY")
+	secretKey := os.Getenv("MINIO_SECRET_KEY")
+
+	// log.Printf("Endpoint: %s", endpoint)
+	// log.Printf("AccessKeyID: %s", accessKey)
+	// log.Printf("SecretAccessKey: %s", secretKey)
+	// Initialize minio client object.
+	var err error
+	MC, err = minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
+		Secure: false,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("%#v\n", MC) // minioClient is now setup
+}
+
+func CheckBuckets(ctx context.Context) error {
+	fmt.Println("checkout buckets")
+	buckets, err := MC.ListBuckets(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	for _, bucket := range buckets {
+		fmt.Println(bucket)
+	}
+	return nil
+}
+
+func PutFile(ctx context.Context, logger *zap.SugaredLogger, file *multipart.FileHeader) (pgtype.UUID, error) {
+	file_size := file.Size
+	object, err := file.Open()
+	if err != nil {
+		return pgtype.UUID{}, err
+	}
+	defer object.Close()
+
+	id := uuid.New()
+	info, err := MC.PutObject(ctx, constants.BUCKETNAME, id.String(), object, file_size, minio.PutObjectOptions{ContentType: "multipart/form-data"})
+	if err != nil {
+		fmt.Println(err)
+		return pgtype.UUID{}, err
+	}
+	logger.Info(info)
+
+	return pgtype.UUID{Bytes: id, Valid: true}, nil
+}
+func GeneratePresignedURL(ctx context.Context, logger *zap.SugaredLogger, id string) (string, error) {
+	reqParams := make(url.Values)
+	presignedURL, err := MC.PresignedGetObject(ctx, constants.BUCKETNAME, id, time.Second*24*60*60, reqParams)
+	if err != nil {
+		logger.Error(err)
+		return "", err
+	}
+	return presignedURL.String(), nil
+}
+
+func GetFileURL(c echo.Context, logger *zap.SugaredLogger, id uuid.UUID) string {
+	url, err := GeneratePresignedURL(c.Request().Context(), logger, id.String())
+	if err != nil {
+		logger.Error(err)
+		return ""
+	}
+	return url
+}
