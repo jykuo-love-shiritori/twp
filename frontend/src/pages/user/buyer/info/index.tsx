13aa2a2a06461d1edd6978a5322cbb1fa39638de
UG <ug00257793@gmail.com>
2023-12-08 21:04:29 +0800

    [frontend] userInfo upload img, modify userImg style

--- frontend/src/pages/user/buyer/info/index.tsx
+++ frontend/src/pages/user/buyer/info/index.tsx
@@ -1,20 +1,43 @@
+import { useRef, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import FormItem from '@components/FormItem';
 
 interface BuyerInfoProps {
   name: string;
   email: string;
   address: string;
+  image: File | undefined;
 }
+
+const BuyerItemStyle = {
+  borderRadius: '10px',
+  padding: '10% 10% 5% 10%',
+  border: '1px solid var(--button_border, #34977F)',
+  background: 'var(--button_dark, #135142)',
+};
+
+const userImgStyle = {
+  borderRadius: '50%',
+  width: '100px',
+  height: '100px',
+  cursor: 'pointer',
+  boxShadow: '2px 4px 10px 2px rgba(0, 0, 0, 0.25)',
+};
+
+const labelStyle = {
+  fontSize: '16px',
+  fontWeight: '500',
+  color: '#FFFFFFAA',
+};
 
 const Info = () => {
   //TODO: get the info from existing user
-  const { register, handleSubmit } = useForm<BuyerInfoProps>({
+  const { register, handleSubmit, watch, setValue } = useForm<BuyerInfoProps>({
     defaultValues: {
-      name: 'name',
-      email: 'email',
-      address: 'address',
+      name: 'username',
+      image: undefined,
+      address: 'an address',
+      email: 'thisIsAnEmail@mail.com',
     },
   });
   const OnFormOutput: SubmitHandler<BuyerInfoProps> = (data) => {
@@ -22,22 +45,84 @@ const Info = () => {
     return data;
   };
 
+  // icon upload thing
+  const [image, setImage] = useState<string | undefined>(undefined);
+  const iconOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      //TODO? check file type
+      setValue('image', e.target.files[0]);
+      setImage(URL.createObjectURL(e.target.files[0]));
+    }
+  };
+  const hiddenFileInput = useRef<HTMLInputElement>(null);
+  const handleIconClick = () => {
+    if (hiddenFileInput.current) {
+      hiddenFileInput.current.click();
+    }
+  };
+
   return (
     <div>
       <div className='title'>Personal info</div>
       <hr className='hr' />
       <form onSubmit={handleSubmit(OnFormOutput)}>
-        <FormItem label='Name'>
-          <input {...register('name', { required: true })} />
-        </FormItem>
-        <FormItem label='Email'>
-          <input {...register('email', { required: true })} />
-        </FormItem>
-        <FormItem label='Address'>
-          <input {...register('address', { required: true })} />
-        </FormItem>
-
         <Row>
+          {/* left half */}
+          <Col xs={12} md={6}>
+            <Row className='center' style={{ height: '100%' }}>
+              <Col />
+              <Col xs={8} xl={7}>
+                <div style={BuyerItemStyle}>
+                  {/* upload img */}
+                  <div className='center'>
+                    <input
+                      type='file'
+                      onChange={iconOnChange}
+                      style={{ display: 'none' }}
+                      ref={hiddenFileInput}
+                    />
+                    <img
+                      src={image ? image : '/placeholder/person.png'}
+                      style={userImgStyle}
+                      onClick={handleIconClick}
+                    />
+                  </div>
+                  <div className='center' style={{ paddingTop: '10px' }}>
+                    <h5>{watch('name')}</h5>
+                  </div>
+                </div>
+              </Col>
+              <Col />
+            </Row>
+          </Col>
+
+          {/* right half */}
+          <Col xs={12} md={6}>
+            <Row>
+              <Col xs={12} style={labelStyle}>
+                Userame
+              </Col>
+              <Col xs={12} className='form_item_wrapper'>
+                <input type='text' {...register('name', { required: true })} />
+              </Col>
+
+              <Col xs={12} style={{ ...labelStyle, paddingTop: '24px' }}>
+                Email
+              </Col>
+              <Col xs={12} className='form_item_wrapper'>
+                <input type='text' {...register('email', { required: true })} />
+              </Col>
+
+              <Col xs={12} style={{ ...labelStyle, paddingTop: '24px' }}>
+                Address
+              </Col>
+              <Col xs={12} className='form_item_wrapper'>
+                <textarea {...register('address', { required: true })} />
+              </Col>
+            </Row>
+          </Col>
+
+          {/* submit button */}
           <Col className='disappear_phone' />
           <Col xs={12} md={4} className='form_item_wrapper' style={{ paddingTop: '24px' }}>
             <input type='submit' value='Save' />
--- frontend/src/pages/user/seller/info/index.tsx
+++ frontend/src/pages/user/seller/info/index.tsx
@@ -4,9 +4,9 @@ import TButton from '@components/TButton';
 import { useRef, useState } from 'react';
 
 interface ShopInfoProps {
-  shopName: string;
-  shopIcon: File | undefined;
-  visibility: boolean;
+  name: string;
+  image: File | undefined;
+  enabled: boolean;
   description: string;
 }
 
@@ -22,6 +22,7 @@ const userImgStyle = {
   width: '100px',
   height: '100px',
   cursor: 'pointer',
+  boxShadow: '2px 4px 10px 2px rgba(0, 0, 0, 0.25)',
 };
 
 const labelStyle = {
@@ -34,9 +35,9 @@ const SellerInfo = () => {
   //TODO: get the info from existing shop
   const { register, handleSubmit, watch, setValue } = useForm<ShopInfoProps>({
     defaultValues: {
-      shopName: 'shop name',
-      shopIcon: undefined,
-      visibility: false,
+      name: 'shop name',
+      image: undefined,
+      enabled: false,
       description: 'shop description',
     },
   });
@@ -50,7 +51,7 @@ const SellerInfo = () => {
   const iconOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       //TODO? check file type
-      setValue('shopIcon', e.target.files[0]);
+      setValue('image', e.target.files[0]);
       setImage(URL.createObjectURL(e.target.files[0]));
     }
   };
@@ -88,7 +89,7 @@ const SellerInfo = () => {
                     />
                   </div>
                   <div className='center' style={{ paddingTop: '10px' }}>
-                    <h5>{watch('shopName')}</h5>
+                    <h5>{watch('name')}</h5>
                   </div>
                   <TButton text='View Shop' />
                 </div>
@@ -104,7 +105,7 @@ const SellerInfo = () => {
                 Shop Name
               </Col>
               <Col xs={12} className='form_item_wrapper'>
-                <input type='text' {...register('shopName', { required: true })} />
+                <input type='text' {...register('name', { required: true })} />
               </Col>
 
               <Col xs={12} style={{ ...labelStyle, paddingTop: '24px' }}>
@@ -117,10 +118,10 @@ const SellerInfo = () => {
                     className='center'
                     style={{ ...labelStyle, padding: '0 0 0 24px' }}
                   >
-                    <input type='checkbox' {...register('visibility')} />
+                    <input type='checkbox' {...register('enabled')} />
                   </Col>
                   <Col className='left'>
-                    {watch('visibility')
+                    {watch('enabled')
                       ? 'Your shop is visible to everyone.🌞'
                       : 'Your shop is hidden from everyone.🌚'}
                   </Col>
