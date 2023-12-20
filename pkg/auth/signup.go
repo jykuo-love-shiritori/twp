f0c5491289440c5300b601a231466e9f0cfb3fe1
noobdy <htommyf12@gmail.com>
2023-12-21 01:37:43 +0800

    [backend] added signup validation

--- pkg/auth/signup.go
+++ pkg/auth/signup.go
@@ -3,6 +3,8 @@ package auth
 import (
 	"net/http"
 	"net/mail"
+	"regexp"
+	"unicode"
 
 	"github.com/jykuo-love-shiritori/twp/db"
 	"github.com/labstack/echo/v4"
@@ -15,6 +17,49 @@ type signupParams struct {
 	Password string `json:"password" example:"secretp@ssword123"`
 	Name     string `json:"name" example:"John Doe"`
 	Email    string `json:"email" example:"test@gmail.com"`
+}
+
+func isValidEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
+
+func isValidUsername(username string) bool {
+	if len(username) > 32 {
+		return false
+	}
+
+	r := regexp.MustCompile(`^[A-Za-z0-9]+$`)
+
+	return r.MatchString(username)
+}
+
+func isValidPassword(password string) bool {
+	// bcrypt only allows passwords with length up to 72 bytes
+	if len(password) < 8 || len([]byte(password)) >= 72 {
+		return false
+	}
+
+	// golang `regexp` doesn't support backtracking
+	hasLower := false
+	hasUpper := false
+	hasNumber := false
+	hasSpecial := false
+
+	for _, v := range password {
+		switch {
+		case unicode.IsLower(v):
+			hasLower = true
+		case unicode.IsUpper(v):
+			hasUpper = true
+		case unicode.IsNumber(v):
+			hasNumber = true
+		case unicode.IsPunct(v) || unicode.IsSymbol(v):
+			hasSpecial = true
+		}
+	}
+
+	return hasLower && hasUpper && hasNumber && hasSpecial
 }
 
 // Signup
@@ -59,10 +104,14 @@ func Signup(pg *db.DB, logger *zap.SugaredLogger) echo.HandlerFunc {
 			return echo.NewHTTPError(http.StatusBadRequest, "Username or email already exists")
 		}
 
-		_, err = mail.ParseAddress(params.Email)
-		if err != nil {
-			logger.Error(err)
+		if !isValidEmail(params.Email) {
 			return echo.NewHTTPError(http.StatusBadRequest, "Invalid email")
+		}
+		if !isValidUsername(params.Username) {
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid username")
+		}
+		if !isValidPassword(params.Password) {
+			return echo.NewHTTPError(http.StatusBadRequest, "Invalid password")
 		}
 
 		hash, err := bcrypt.GenerateFromPassword([]byte(params.Password), 14)
