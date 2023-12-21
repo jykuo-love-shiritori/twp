47e8867a5bb3a5327cb89b535b66800303b97f64
tyk <b0984017369@gmail.com>
2023-12-22 00:53:59 +0800

    [frontend] change to useForm

--- frontend/src/pages/user/seller/allReports/index.tsx
+++ frontend/src/pages/user/seller/allReports/index.tsx
@@ -1,52 +1,74 @@
-import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
-
-import TButton from '@components/TButton';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import FormItem from '@components/FormItem';
 
+interface Props {
+  year: number;
+  month: number;
+}
+
 const SellerReport = () => {
+  const navigate = useNavigate();
+  const baseYear = 2020;
   const currentYear = new Date().getFullYear();
-  const years = Array.from({ length: currentYear - 2019 }, (_, index) => 2020 + index);
+  const years = Array.from(
+    { length: currentYear - baseYear + 1 },
+    (_, index) => currentYear - index,
+  );
   const months = Array.from({ length: 12 }, (_, index) => index + 1);
 
-  const [year, setYear] = useState(years[0]);
-  const [month, setMonth] = useState(months[0]);
+  const { register, handleSubmit } = useForm<Props>({
+    defaultValues: {
+      year: years[0],
+      month: months[0],
+    },
+  });
+
+  const onSubmit: SubmitHandler<Props> = (data) => {
+    navigate(`/user/seller/reports/${data.year}/${data.month}`);
+  };
 
   return (
     <div style={{ padding: '5% 8% 10% 8% ' }}>
       <h3>Select report period</h3>
       <hr className='hr' />
 
-      <FormItem label='Year'>
-        <Form.Select
-          aria-label='Year'
-          value={year}
-          onChange={(e) => setYear(parseInt(e.target.value))}
-        >
-          <option value=''>Select year</option>
-          {years.map((y) => (
-            <option key={y} value={y}>
-              {y}
-            </option>
-          ))}
-        </Form.Select>
-      </FormItem>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormItem label='Year'>
+          <Form.Select
+            {...register('year', {
+              setValueAs: (value) => parseInt(value),
+            })}
+            aria-label='Year'
+          >
+            {years.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </Form.Select>
+        </FormItem>
 
-      <FormItem label='Month'>
-        <Form.Select
-          aria-label='Month'
-          value={month}
-          onChange={(e) => setMonth(parseInt(e.target.value))}
-        >
-          <option>Select month</option>
-          {months.map((m) => (
-            <option key={m} value={m}>
-              {m}
-            </option>
-          ))}
-        </Form.Select>
-      </FormItem>
-      <TButton text='Confirm' action={`/user/seller/reports/${year}/${month}`} />
+        <FormItem label='Month'>
+          <Form.Select
+            {...register('month', {
+              setValueAs: (value) => parseInt(value),
+            })}
+            aria-label='Month'
+          >
+            {months.map((m) => (
+              <option key={m} value={m}>
+                {m}
+              </option>
+            ))}
+          </Form.Select>
+        </FormItem>
+
+        <div className='form_item_wrapper'>
+          <input type='submit' value='Confirm' />
+        </div>
+      </form>
     </div>
   );
 };
