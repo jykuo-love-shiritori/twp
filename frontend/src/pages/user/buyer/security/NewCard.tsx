87093b74c190a57e7873fe7aec8f943b7b3bd03b
ug <ug00257793@gmail.com>
2023-12-25 04:57:27 +0800

    [frontend] credit card apis

--- frontend/src/pages/user/buyer/security/NewCard.tsx
+++ frontend/src/pages/user/buyer/security/NewCard.tsx
@@ -1,40 +1,96 @@
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import FormItem from '@components/FormItem';
+import { RouteOnNotOK } from '@lib/Functions';
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { CheckFetchStatus } from '@lib/Status';
 
-import TButton from '@components/TButton';
-import PasswordItem from '@components/PasswordItem';
-import InfoItem from '@components/InfoItem';
+interface ICreditCard {
+  CVV: string;
+  name: string;
+  card_number: string;
+  expiry_date: string;
+}
 
 const NewCard = () => {
-  const [creditCardNumber, setCreditCardNumber] = useState<string>('');
-  const [CVV, setCVV] = useState<string>('');
-  const [expirationDate, setExpirationDate] = useState<string>('');
-  const [cardHolderName, setCardHolderName] = useState<string>('');
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<ICreditCard>();
+  const onSubmit = async (data: ICreditCard) => {
+    const resp = await fetch('/api/user/security/credit_card', {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(oldCards ? oldCards.concat(data) : [data]),
+    });
+    if (!resp.ok) {
+      RouteOnNotOK(resp, navigate);
+    } else {
+      navigate('/user/security/manageCreditCard');
+    }
+  };
+
+  const { data: oldCards, status } = useQuery({
+    queryKey: ['userGetCreditCard'],
+    queryFn: async () => {
+      const resp = await fetch('/api/user/security/credit_card');
+      RouteOnNotOK(resp, navigate);
+      return await resp.json();
+    },
+    select: (data) => data as [ICreditCard],
+    retry: false,
+    enabled: true,
+    refetchOnWindowFocus: false,
+  });
+  if (status !== 'success') {
+    return <CheckFetchStatus status={status} />;
+  }
 
   return (
-    <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className='title'>Security - Credit Card</div>
       <hr className='hr' />
-      <InfoItem
-        text='CreditCard Number'
-        isMore={false}
-        value={creditCardNumber}
-        setValue={setCreditCardNumber}
-      />
-      <PasswordItem text='Security Code (CVV)' value={CVV} setValue={setCVV} />
-      <InfoItem
-        text='Expiration Date'
-        isMore={false}
-        value={expirationDate}
-        setValue={setExpirationDate}
-      />
-      <InfoItem
-        text='Cardholder Name'
-        isMore={false}
-        value={cardHolderName}
-        setValue={setCardHolderName}
-      />
-      <TButton text='Save' />
-    </div>
+      <FormItem label='Card Name'>
+        <input type='text' placeholder='Card Name' {...register('name', { required: true })} />
+      </FormItem>
+      <FormItem label='Card Number'>
+        <input
+          type='text'
+          placeholder='Card Number'
+          {...register('card_number', {
+            required: true,
+            pattern: {
+              value: /^\d{4} \d{4} \d{4} \d{4}$/,
+              message: 'must be like: "XXXX XXXX XXXX XXXX"',
+            },
+          })}
+        />
+      </FormItem>
+      <FormItem label='Security Code (CVV)'>
+        <input
+          type='text'
+          placeholder='Security Code (CVV)'
+          {...register('CVV', {
+            required: true,
+            pattern: { value: /^\d{3}$/, message: 'must be 3 digits' },
+          })}
+        />
+      </FormItem>
+      <FormItem label='Expiration Date'>
+        <input
+          type='text'
+          placeholder='Expiration Date'
+          {...register('expiry_date', {
+            required: true,
+            pattern: {
+              value: /^\d{2}\/\d{2}$/,
+              message: 'must be like: "MM/YY"',
+            },
+          })}
+        />
+      </FormItem>
+      <div className='form_item_wrapper'>
+        <input type='submit' value='Save' />
+      </div>
+    </form>
   );
 };
 
