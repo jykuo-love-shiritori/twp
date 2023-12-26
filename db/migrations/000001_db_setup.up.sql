ebe747b7197fe8ba4cec94205ef071a6744e8f3c
catJam <loginerrore04@gmail.com>
2023-12-26 14:29:59 +0800

    [backend] FixNull `image_id` handling #103

--- db/migrations/000001_db_setup.up.sql
+++ db/migrations/000001_db_setup.up.sql
@@ -1,4 +1,4 @@
-CREATE TYPE "order_status" AS ENUM (
+CREATE TYPE "order_status" AS ENUM(
     'paid',
     'shipped',
     'delivered',
@@ -6,41 +6,41 @@ CREATE TYPE "order_status" AS ENUM (
     'finished'
 );
 
-CREATE TYPE "coupon_type" AS ENUM (
+CREATE TYPE "coupon_type" AS ENUM(
     'percentage',
     'fixed',
     'shipping'
 );
 
-CREATE TYPE "coupon_scope" AS ENUM (
+CREATE TYPE "coupon_scope" AS ENUM(
     'global',
     'shop'
 );
 
-CREATE TYPE "role_type" AS ENUM (
+CREATE TYPE "role_type" AS ENUM(
     'admin',
     'customer'
 );
 
-CREATE TABLE "cart" (
+CREATE TABLE "cart"(
     "id" SERIAL PRIMARY KEY,
     "user_id" INT NOT NULL,
     "shop_id" INT NOT NULL,
     CONSTRAINT unique_shop_user UNIQUE ("shop_id", "user_id")
 );
 
-CREATE TABLE "cart_product" (
+CREATE TABLE "cart_product"(
     "cart_id" INT NOT NULL,
     "product_id" INT NOT NULL,
     "quantity" INT NOT NULL,
     CONSTRAINT unique_cart_product UNIQUE ("cart_id", "product_id")
 );
 
-CREATE TABLE "order_history" (
+CREATE TABLE "order_history"(
     "id" SERIAL PRIMARY KEY,
     "user_id" INT NOT NULL,
     "shop_id" INT NOT NULL,
-    "image_id" TEXT,
+    "image_id" TEXT NOT NULL,
     -- fot thumbnail
     "shipment" INT NOT NULL,
     "total_price" INT NOT NULL,
@@ -48,7 +48,7 @@ CREATE TABLE "order_history" (
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE "order_detail" (
+CREATE TABLE "order_detail"(
     "order_id" INT NOT NULL,
     "product_id" INT NOT NULL,
     "product_version" INT NOT NULL,
@@ -56,13 +56,13 @@ CREATE TABLE "order_detail" (
     PRIMARY KEY ("order_id", "product_id", "product_version")
 );
 
-CREATE TABLE "cart_coupon" (
+CREATE TABLE "cart_coupon"(
     "cart_id" INT NOT NULL,
     "coupon_id" INT NOT NULL,
     CONSTRAINT unique_cart_coupon UNIQUE ("cart_id", "coupon_id")
 );
 
-CREATE TABLE "product" (
+CREATE TABLE "product"(
     "id" SERIAL PRIMARY KEY,
     "version" INT NOT NULL,
     "shop_id" INT NOT NULL,
@@ -78,17 +78,17 @@ CREATE TABLE "product" (
     "enabled" BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE "product_archive" (
+CREATE TABLE "product_archive"(
     "id" INT NOT NULL,
     "version" INT NOT NULL DEFAULT 1,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "price" DECIMAL(10, 2) NOT NULL,
-    "image_id" TEXT,
+    "image_id" TEXT NOT NULL,
     PRIMARY KEY ("id", "version")
 );
 
-CREATE TABLE "coupon" (
+CREATE TABLE "coupon"(
     "id" SERIAL PRIMARY KEY,
     "type" coupon_type NOT NULL,
     "scope" coupon_scope NOT NULL,
@@ -100,98 +100,98 @@ CREATE TABLE "coupon" (
     "expire_date" TIMESTAMPTZ NOT NULL CHECK ("expire_date" > "start_date")
 );
 
-CREATE TABLE "user" (
+CREATE TABLE "user"(
     "id" SERIAL PRIMARY KEY,
     "username" TEXT NOT NULL UNIQUE,
     "password" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL UNIQUE,
     "address" TEXT NOT NULL,
-    "image_id" TEXT,
+    "image_id" TEXT NOT NULL,
     "role" role_type NOT NULL,
     "credit_card" JSONB NOT NULL,
     "refresh_token" TEXT NULL,
     "enabled" BOOLEAN NOT NULL DEFAULT TRUE -- if user deleted, set enabled to false
 );
 
-CREATE TABLE "shop" (
+CREATE TABLE "shop"(
     "id" SERIAL PRIMARY KEY,
     "seller_name" TEXT NOT NULL,
-    "image_id" TEXT,
+    "image_id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "enabled" BOOLEAN NOT NULL
 );
 
-CREATE TABLE "tag" (
+CREATE TABLE "tag"(
     "id" SERIAL PRIMARY KEY,
     "shop_id" INT NOT NULL,
     "name" TEXT NOT NULL,
     CONSTRAINT unique_name_shop UNIQUE ("shop_id", "name")
 );
 
-CREATE TABLE "product_tag" (
+CREATE TABLE "product_tag"(
     "tag_id" INT NOT NULL,
     "product_id" INT NOT NULL,
     CONSTRAINT unique_tag_product UNIQUE ("tag_id", "product_id")
 );
 
-CREATE TABLE "coupon_tag" (
+CREATE TABLE "coupon_tag"(
     "coupon_id" INT NOT NULL,
     "tag_id" INT NOT NULL,
     CONSTRAINT unique_tag_coupon UNIQUE ("tag_id", "coupon_id")
 );
 
 ALTER TABLE "shop"
-    ADD FOREIGN KEY ("seller_name") REFERENCES "user" ("username");
+    ADD FOREIGN KEY ("seller_name") REFERENCES "user"("username");
 
 ALTER TABLE "product"
-    ADD FOREIGN KEY ("shop_id") REFERENCES "shop" ("id");
+    ADD FOREIGN KEY ("shop_id") REFERENCES "shop"("id");
 
 ALTER TABLE "coupon"
-    ADD FOREIGN KEY ("shop_id") REFERENCES "shop" ("id");
+    ADD FOREIGN KEY ("shop_id") REFERENCES "shop"("id");
 
 ALTER TABLE "tag"
-    ADD FOREIGN KEY ("shop_id") REFERENCES "shop" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("shop_id") REFERENCES "shop"("id") ON DELETE CASCADE;
 
 ALTER TABLE "product_tag"
-    ADD FOREIGN KEY ("product_id") REFERENCES "product" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE CASCADE;
 
 ALTER TABLE "product_tag"
-    ADD FOREIGN KEY ("tag_id") REFERENCES "tag" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("tag_id") REFERENCES "tag"("id") ON DELETE CASCADE;
 
 ALTER TABLE "coupon_tag"
-    ADD FOREIGN KEY ("coupon_id") REFERENCES "coupon" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("coupon_id") REFERENCES "coupon"("id") ON DELETE CASCADE;
 
 ALTER TABLE "coupon_tag"
-    ADD FOREIGN KEY ("tag_id") REFERENCES "tag" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("tag_id") REFERENCES "tag"("id") ON DELETE CASCADE;
 
 ALTER TABLE "cart"
-    ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE;
 
 ALTER TABLE "cart"
-    ADD FOREIGN KEY ("shop_id") REFERENCES "shop" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("shop_id") REFERENCES "shop"("id") ON DELETE CASCADE;
 
 ALTER TABLE "cart_product"
-    ADD FOREIGN KEY ("cart_id") REFERENCES "cart" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("cart_id") REFERENCES "cart"("id") ON DELETE CASCADE;
 
 ALTER TABLE "cart_product"
-    ADD FOREIGN KEY ("product_id") REFERENCES "product" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("product_id") REFERENCES "product"("id") ON DELETE CASCADE;
 
 ALTER TABLE "cart_coupon"
-    ADD FOREIGN KEY ("cart_id") REFERENCES "cart" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("cart_id") REFERENCES "cart"("id") ON DELETE CASCADE;
 
 ALTER TABLE "cart_coupon"
-    ADD FOREIGN KEY ("coupon_id") REFERENCES "coupon" ("id") ON DELETE CASCADE;
+    ADD FOREIGN KEY ("coupon_id") REFERENCES "coupon"("id") ON DELETE CASCADE;
 
 ALTER TABLE "order_detail"
-    ADD FOREIGN KEY ("order_id") REFERENCES "order_history" ("id");
+    ADD FOREIGN KEY ("order_id") REFERENCES "order_history"("id");
 
 ALTER TABLE "order_detail"
-    ADD FOREIGN KEY ("product_id", "product_version") REFERENCES "product_archive" ("id", "version");
+    ADD FOREIGN KEY ("product_id", "product_version") REFERENCES "product_archive"("id", "version");
 
 ALTER TABLE "order_history"
-    ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
+    ADD FOREIGN KEY ("user_id") REFERENCES "user"("id");
 
 ALTER TABLE "order_history"
-    ADD FOREIGN KEY ("shop_id") REFERENCES "shop" ("id");
+    ADD FOREIGN KEY ("shop_id") REFERENCES "shop"("id");
