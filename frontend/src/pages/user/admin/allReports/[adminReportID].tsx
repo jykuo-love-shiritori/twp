7629e1e332ff530e0430eb27de9db308e03525b1
tyk <b0984017369@gmail.com>
2023-12-27 13:39:45 +0800

    [frontend] seller manage products, admin report GET

--- frontend/src/pages/user/admin/allReports/[adminReportID].tsx
+++ frontend/src/pages/user/admin/allReports/[adminReportID].tsx
@@ -2,25 +2,18 @@ import '@style/global.css';
 
 import { Col, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 
 import SellerItem from '@components/SellerItem';
 import NotFound from '@components/NotFound';
 
-import adminReportData from '@pages/user/admin/adminReportData.json';
+import { CheckFetchStatus } from '@lib/Status';
 
-interface Sellers {
+interface SellersProps {
   id: number;
   name: string;
   imgUrl: string;
   amount: number;
-}
-
-interface Props {
-  year: number;
-  month: number;
-  sellers: Sellers[];
-  totalAmount: number;
-  id: number;
 }
 
 const AdminReportEach = () => {
@@ -33,57 +26,79 @@ const AdminReportEach = () => {
   };
 
   const { year, month } = useParams();
+  const yearString = year ?? '';
+  const monthString = month ?? '';
 
-  const data: Props | undefined = adminReportData.find(
-    (item) => item.year.toString() === year && item.month.toString() === month,
-  );
+  const rfc3339Date = new Date(
+    Date.UTC(parseInt(yearString), parseInt(monthString) - 1, 1),
+  ).toISOString();
 
-  if (data) {
-    return (
-      <div style={{ padding: '10% 10% 0% 10%' }}>
-        <div className='flex_wrapper' style={reportPageStyle}>
-          <h1 className='title_color' style={{ paddingBottom: '30px' }}>
-            <b>
-              {data.year} / {data.month} Finical Report
-            </b>
-          </h1>
-          <h4>The top three highest monthly sales</h4>
-          <hr className='hr' />
-          <Row>
-            {data.sellers.map((item, index) => {
-              return (
-                <Col xs={12} md={4} key={index}>
-                  <div className='center title_color' style={{ paddingTop: '30px' }}>
-                    <h4>No. {index + 1}</h4>
-                  </div>
-                  <div className='center' style={{ paddingBottom: '30px' }}>
-                    TWD $ {item.amount}
-                  </div>
+  const { status, data: adminReport } = useQuery({
+    queryKey: ['adminReport'],
+    queryFn: async () => {
+      const response = await fetch(`/api/admin/report?date=${rfc3339Date}`, {
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    },
+  });
 
-                  <SellerItem id={item.id} />
-                </Col>
-              );
-            })}
-          </Row>
+  if (status != 'success') {
+    return <CheckFetchStatus status={status} />;
+  }
 
-          <hr className='hr' />
-
-          <Row>
-            <Col xs={12} className='center_vertical'>
-              <h4>
-                <b>Total sales revenue :</b>
-              </h4>
-            </Col>
-            <Col xs={12} className='center_vertical'>
-              TWD $ {data.totalAmount}
-            </Col>
-          </Row>
-        </div>
-      </div>
-    );
-  } else {
+  if (adminReport.sellers.length === 0 && adminReport.total == 0) {
+    console.log('no data');
     return <NotFound />;
   }
+
+  return (
+    <div style={{ padding: '10% 10% 0% 10%' }}>
+      <div className='flex_wrapper' style={reportPageStyle}>
+        <h1 className='title_color' style={{ paddingBottom: '30px' }}>
+          <b>
+            {adminReport.year} / {adminReport.month} Finical Report
+          </b>
+        </h1>
+        <h4>The top three highest monthly sales</h4>
+        <hr className='hr' />
+        <Row>
+          {adminReport.sellers.map((item: SellersProps, index: number) => {
+            return (
+              <Col xs={12} md={4} key={index}>
+                <div className='center title_color' style={{ paddingTop: '30px' }}>
+                  <h4>No. {index + 1}</h4>
+                </div>
+                <div className='center' style={{ paddingBottom: '30px' }}>
+                  TWD $ {item.amount}
+                </div>
+
+                <SellerItem id={item.id} />
+              </Col>
+            );
+          })}
+        </Row>
+
+        <hr className='hr' />
+
+        <Row>
+          <Col xs={12} className='center_vertical'>
+            <h4>
+              <b>Total sales revenue :</b>
+            </h4>
+          </Col>
+          <Col xs={12} className='center_vertical'>
+            TWD $ {adminReport.total}
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 };
 
 export default AdminReportEach;
--- frontend/src/pages/user/seller/allProducts/index.tsx
+++ frontend/src/pages/user/seller/allProducts/index.tsx
@@ -1,11 +1,31 @@
 import { Col, Row } from 'react-bootstrap';
+import { useQuery } from '@tanstack/react-query';
 
 import SellerGoodsItem from '@components/SellerGoodsItem';
 import TButton from '@components/TButton';
-
-import goodsData from '@pages/discover/goodsData.json';
+import { CheckFetchStatus } from '@lib/Status';
+import { GoodsItemProps } from '@components/GoodsItem';
 
 const Products = () => {
+  const { status, data: sellerShopData } = useQuery({
+    queryKey: ['sellerShopView'],
+    queryFn: async () => {
+      const response = await fetch(`/api/seller/product?offset=${0}&limit=${8}`, {
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    },
+  });
+
+  if (status != 'success') {
+    return <CheckFetchStatus status={status} />;
+  }
+
   return (
     <div>
       <Row>
@@ -20,15 +40,10 @@ const Products = () => {
       </Row>
       <hr className='hr' />
       <Row>
-        {goodsData.map((data, index) => {
+        {sellerShopData.map((data: GoodsItemProps, index: number) => {
           return (
             <Col xs={6} md={3} key={index}>
-              <SellerGoodsItem
-                id={data.id}
-                name={data.name}
-                image_url={data.image_url}
-                isIndex={false}
-              />
+              <SellerGoodsItem id={data.id} name={data.name} image_url={data.image_url} />
             </Col>
           );
         })}
