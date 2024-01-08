deb2d455766cf5aaf056d7c3e5553793b5b6151b
tyk <b0984017369@gmail.com>
2024-01-08 22:54:31 +0800

    [frontend] navbar search works
    Co-authored-by: NOOBDY <48180490+NOOBDY@users.noreply.github.com>

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -4,46 +4,53 @@ import '@style/global.css';
 import { Row, Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useState } from 'react';
+import { FormEventHandler, useEffect, useState } from 'react';
 
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams, useSearchParams } from 'react-router-dom';
 
 import { SearchProps } from '@pages/search';
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const getUrl = (data: SearchProps, q: string) => {
-  let url: string = '/search?q=' + q;
-  url += getBehind(data);
-  return url;
+  const params = new URLSearchParams();
+  params.set('q', q);
+  return '/search?' + setFilter(data, params).toString();
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const getBehind = (data: SearchProps) => {
-  let url = '';
+export const setFilter = (data: SearchProps, params: URLSearchParams) => {
+  if (data.minPrice) params.set('minPrice', data.minPrice.toString());
+  if (data.maxPrice) params.set('maxPrice', data.maxPrice.toString());
+  if (data.minStock) params.set('minStock', data.minStock.toString());
+  if (data.maxStock) params.set('maxStock', data.maxStock.toString());
+  if (data.haveCoupon) params.set('haveCoupon', data.haveCoupon.toString());
+  if (data.sortBy) params.set('sortBy', data.sortBy);
+  if (data.order) params.set('order', data.order);
+  return params;
 
-  if (data.minPrice !== null && data.minPrice.toString() !== '') {
-    url += '&minPrice=' + data.minPrice;
-  }
-  if (data.maxPrice !== null && data.maxPrice.toString() !== '') {
-    url += '&maxPrice=' + data.maxPrice;
-  }
-  if (data.minStock !== null) {
-    url += '&minStock=' + data.minStock;
-  }
-  if (data.maxStock !== null) {
-    url += '&maxStock=' + data.maxStock;
-  }
-  if (data.haveCoupon !== null) {
-    url += '&haveCoupon=' + data.haveCoupon.toString();
-  }
-  if (data.sortBy !== null) {
-    url += '&sortBy=' + data.sortBy;
-  }
-  if (data.order !== null) {
-    url += '&order=' + data.order;
-  }
-  url = url.replace(/ /g, '%20');
-  return url;
+  // if (data.minPrice !== null && data.minPrice.toString() !== '') {
+  //   url += '&minPrice=' + data.minPrice;
+  // }
+  // if (data.maxPrice !== null && data.maxPrice.toString() !== '') {
+  //   url += '&maxPrice=' + data.maxPrice;
+  // }
+  // if (data.minStock !== null) {
+  //   url += '&minStock=' + data.minStock;
+  // }
+  // if (data.maxStock !== null) {
+  //   url += '&maxStock=' + data.maxStock;
+  // }
+  // if (data.haveCoupon !== null) {
+  //   url += '&haveCoupon=' + data.haveCoupon.toString();
+  // }
+  // if (data.sortBy !== null) {
+  //   url += '&sortBy=' + data.sortBy;
+  // }
+  // if (data.order !== null) {
+  //   url += '&order=' + data.order;
+  // }
+  // url = url.replace(/ /g, '%20');
+  // return url;
 };
 
 const SearchBar = () => {
@@ -51,67 +58,71 @@ const SearchBar = () => {
   const location = useLocation();
   const sellerNameFromParams = useParams().sellerName;
   const [sellerName, setSellerName] = useState<string | undefined>(initialSellerName);
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
     setSellerName(sellerNameFromParams);
   }, [location, sellerNameFromParams]);
 
   const navigate = useNavigate();
-  const [q, setQ] = useState<string>('');
+  const [q, setQ] = useState<string>(searchParams.get('q') ?? '');
 
-  const transfer = () => {
-    if (q === '') {
+  const transfer: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    if (!q) {
       return;
     }
 
-    let fullUrl = '';
-    const searchQuery = q ? `?q=${encodeURIComponent(q)}` : '';
+    const params = new URLSearchParams({ q: q });
+
+    console.log(params.toString());
 
     const isShopURL = /\/shop\/(?:[^/]+\/)?products(?:\/inside\/search(?:\?.+)?)?$/;
-    if (isShopURL.test(window.location.pathname)) {
-      if (sellerName !== undefined) {
-        fullUrl = `${`/shop/${sellerName}/products/inside/search`}${searchQuery}`;
-      }
-    } else {
-      fullUrl = `${`/search`}${searchQuery}`;
+    if (isShopURL.test(window.location.pathname) && sellerName) {
+      navigate(`/shop/${sellerName}/products/inside/search?${params.toString()}`);
+      return;
     }
-    navigate(fullUrl);
+
+    const url = `/search?${params.toString()}`;
+    navigate(url);
   };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.keyCode === 229) return;
+  // const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  //   if (event.keyCode === 229) return;
 
-    if (event.key === 'Enter') {
-      transfer();
-    }
-  };
+  //   if (event.key === 'Enter') {
+  //     transfer();
+  //   }
+  // };
 
-  const handleClick = () => {
-    transfer();
-  };
+  // const handleClick = () => {
+  //   transfer();
+  // };
 
   return (
     <div>
-      <Row>
-        <Col xs={12} sm={12} md={12} lg={8} className='center'>
-          <div className='input_container'>
-            <input
-              type='text'
-              placeholder='Search'
-              value={q}
-              onChange={(e) => setQ(e.target.value)}
-              onKeyDown={handleKeyDown}
-              className='search'
-            />
-            <FontAwesomeIcon icon={faSearch} className='search_icon' />
-          </div>
-        </Col>
-        <Col xs={4} lg={4} className='disappear_tablet disappear_phone'>
-          <Button className='search_button center' onClick={handleClick}>
-            Search
-          </Button>
-        </Col>
-      </Row>
+      <form onSubmit={transfer}>
+        <Row>
+          <Col xs={12} sm={12} md={12} lg={8} className='center'>
+            <div className='input_container'>
+              <input
+                type='text'
+                placeholder='Search'
+                value={q}
+                onChange={(e) => setQ(e.target.value)}
+                className='search'
+              />
+              <FontAwesomeIcon icon={faSearch} className='search_icon' />
+            </div>
+          </Col>
+          <Col xs={4} lg={4} className='disappear_tablet disappear_phone'>
+            <Button className='search_button center' type='submit'>
+              Search
+            </Button>
+          </Col>
+        </Row>
+      </form>
     </div>
   );
 };
