8f19c2bff32158344382444d8430a5586dd960b5
tyk <b0984017369@gmail.com>
2024-01-08 10:56:15 +0800

    [frontend] fix bug

--- frontend/src/components/UserTableRow.tsx
+++ frontend/src/components/UserTableRow.tsx
@@ -31,7 +31,6 @@ const userImgStyle: CSSProperties = {
   maxHeight: '7vh',
   maxWidth: '7vh',
   objectFit: 'cover',
-  cursor: 'pointer',
   boxShadow: '2px 4px 10px 2px rgba(0, 0, 0, 0.25)',
 };
 
